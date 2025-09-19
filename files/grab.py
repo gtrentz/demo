a3615b37c1cc6c52af6664b93646ebb90cb5a0f7
gtrentz <grant-trentz@uiowa.edu>
2025-09-18 21:14:26 -0500

    Web test

---
+++ files/grab.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import requests
+import json
+
+app = Flask(__name__)
+
+def get_image():
+    url = "https://mem-api.herokuapp.com/gimme"
+    response = json.loads(requests.request("GET", url).text)
+    meme_large = response["preview"][-2]
+    subreddit = response["subreddit"]
+    return meme_large, subreddit
+
+@app.route("/")
+def index():
+    meme_pic, subreddit = get_image()
+    return render_template("meme_index.html", meme_pic = meme_pic, subreddit=subreddit)
+
+app.run(host="0.0.0.0", port=80)
